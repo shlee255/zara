ce20b135b5a64f55812d50c23acb1825d0ccf1a1
Hong <hong.lee@energybox.com>
2022-11-09 11:29:13 +0800

    Overview

--- Course Project/project.py
+++ Course Project/project.py
@@ -1,7 +1,30 @@
-with open('COMP1012-Project-data/driving-records/detail_record_2017_01_02_08_00_00')  as csvfile:
-	lines = csvfile.read().splitlines()
+datas = []
+drivers = {}
 
-#for line in lines:
-#	print(line)
+def overview(filename):
+	with open(filename)  as csvfile:
+		lines = csvfile.read().splitlines()
 
-print("Number of Tecords: " + str(len(lines)))
+	for line in lines:
+		datas.append(line.split(","))
+
+	for data in datas:
+		if data[0] in drivers:
+			drivers[data[0]] = drivers[data[0]] + 1
+		else:
+			drivers[data[0]] = 1
+
+	print(drivers)
+	print(filename)
+	print("Number of Records: " + str(len(lines)) + ", Number of Drivers: " + str(len(drivers)))
+
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_02_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_03_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_04_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_05_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_06_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_07_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_08_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_09_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_10_08_00_00')
+overview('COMP1012-Project-data/driving-records/detail_record_2017_01_11_08_00_00')
