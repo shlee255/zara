7db01d4ce8e831bb36e6b8893806bf5972e3b380
Hong <hong.lee@energybox.com>
2022-11-17 16:31:42 +0800

    Implementing Speed plot

--- Course Project/project.py
+++ Course Project/project.py
@@ -25,7 +25,8 @@ def overview(filename):
 			"isFatigueDriving",
             "isHthrottleStop",
 			"isOilLeak",
-			"other"]).fillna(value = 0)
+			"other"]) #,
+            # index_col="Time", parse_dates=True)#.fillna(value = 0)
 
 	print("There are " + str(len(df.driverID)) + " records and " + str(len(df.driverID.unique())) + " drivers in " + filename)
 	#print(df)
@@ -50,20 +51,59 @@ print("There are " + str(len(dataset.driverID)) + " records and " + str(len(data
 
 print(dataset)
 
+# Plot Driver vs Speed
+speedData = dataset[["Time", "driverID", "Speed"]]
+speedData = speedData.drop_duplicates(["Time","driverID"])
+speedData = speedData.set_index("Time")
+print(speedData)
+speed_pivot = speedData.pivot(columns='driverID',values='Speed')
+print(speed_pivot)
+
+speed_pivot.to_csv('speed.csv')
+
+#speedData = dataset[['Time',"driverID","Speed"]]
+#print(speedData)
+
+#speed_subset = speedData[['driverID','Speed']].sort_index().groupby(['driverID'])
+#print(speed_subset)
+
+# dataset.set_index(['Time','driverID'],append=True)
+#Test = dataset[["Time", "driverID", "Speed"]]
+#Test.set_index(["Time","driverID"], append=True)
+#Test.reset_index()
+
+#Test = Test.drop_duplicates(["Time","driverID"])
+#Test = Test.set_index("Time")
+
+#print(Test)
+
+#data_pivot = Test.pivot(columns='driverID',values='Speed')# dataset.pivot(columns='driverID',values='Speed')
+#print(data_pivot)
+
+
+# speedData.set_index('Time')
+
+#speedData.groupby('driverID')[['Speed']]
+
+#plt.plot(legend=True,title="Speed")
+
+
+
 # speedAnalytics = dataset.groupby("Time")["Speed","carPlateNumber"].agg(Speed=('Speed', 'mean')).reset_index()
 
 # fig, ax = plt.subplots(figsize=(10,5))
 
 # speedAnalytics.set_index('Time', inplace=True)
 
-print(dataset['Time'].dtypes)
 
-dataset['Datetime'] = pd.to_datetime(dataset['Time'])
-dataset.set_index('Datetime')
-dataset.groupby('driverID')[['Speed']].plot(legend=True,title="Speed")
+# print(dataset['Time'].dtypes)
+
+# dataset['Datetime'] = pd.to_datetime(dataset['Time'])
+# dataset.set_index('Datetime')
+# dataset.groupby('driverID')[['Speed']].plot(legend=True,title="Speed")
 
 #print(dataset['Datetime'].dtypes)
 #print(dataset.values.tolist())
 #print(dataset.groupby(['driverID'])[['Datetime','Speed']].values.tolist())
 #plt.plot()
-plt.show()
+#plt.show()
